c5d40871c401ace6ca6cdb6b2a6552a9402511e7
Edi Santoso <edicyber@gmail.com>
2014-10-06 06:19:27 +0630

    Revert "Revert "config database""
    This reverts commit cb47c29887ec42160933b16cd435e0601db2ef3f.

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'simdes_2014',
+			'database'  => 'appbudget',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
