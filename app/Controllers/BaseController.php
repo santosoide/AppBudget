756504c6964a17aee8c26a217fef91f2e59e87e6
Edi Santoso <edicyber@gmail.com>
2014-10-08 07:43:55 +0630

    menambahkan beberapa function

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,18 +1,98 @@
 <?php
 
-class BaseController extends Controller {
+class BaseController extends Controller
+{
+    /**
+     * @var string
+     */
+    protected $layout = 'layouts.default';
 
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
+    /**
+     * @var
+     */
+    protected $auth;
 
+    /**
+     * @var
+     */
+    protected $input;
+
+    /**
+     * @var Redirect
+     */
+    protected $redirect;
+
+    /**
+     * @param Input $input
+     * @param Redirect $redirect
+     */
+    public function __construct(
+        Input $input,
+        Redirect $redirect
+    )
+    {
+        $this->input = $input;
+        $this->redirect = $redirect;
+    }
+
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function setupLayout()
+    {
+        if (!is_null($this->layout)) {
+            $this->layout = View::make($this->layout);
+        }
+    }
+
+    /**
+     * @param $input
+     * @return mixed
+     */
+    protected function input($input)
+    {
+        return Input::get($input);
+
+//        return $this->input->get($input);
+    }
+
+    /**
+     * @param $route
+     * @param array $params
+     * @param array $data
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function redirectRoute($route, $params = [], $data = [])
+    {
+        return $this->redirect->route($route, $params)->with($data);
+    }
+
+    /**
+     * @param $url
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function redirectURLTo($url)
+    {
+        return $this->redirect->to($url);
+    }
+
+    /**
+     * @param array $data
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function redirectBack($data = [])
+    {
+        return $this->redirect->back()->withInput()->with($data);
+    }
+
+    /**
+     * @param null $default
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function redirectIntended($default = null)
+    {
+        return $this->redirect->intended($default);
+    }
 }
